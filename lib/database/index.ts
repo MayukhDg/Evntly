d7db87e66bba5b51c08e71fe1af881e05ae83c93
Mayukh <mayukh1995@gmail.com>
2024-08-03 17:38:27 +0530

    ...

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,20 +1,32 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+//dbname- Evt
 
-//dbname - Evt
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose
+
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
+}
 
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
+  if(cached.conn) return cached.conn;
 
-  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if(!MONGODB_URI) throw new Error('Missing MONGODB_URL');
 
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'evently',
-    bufferCommands: false,
-  })
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URI, { 
+      dbName: 'evently', bufferCommands: false 
+    })
 
   cached.conn = await cached.promise;
 
